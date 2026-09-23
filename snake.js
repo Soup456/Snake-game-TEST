5a86e02b057933fa0ba7565fcedc1ead2c680ca6
Soup456 <20226@class.tuakaucollege.com>
2026-09-24 09:17:31 +1200

    Update snake.js

--- snake.js
+++ snake.js
@@ -5,70 +5,87 @@ class Snake {
     this.xspeed = 1;
     this.yspeed = 0;
     this.total = 0;
-    this.tail = [];
-  }
-
-  direction(x, y) {
-    if (x !== 0 && this.xspeed === -x) return;
-    if (y !== 0 && this.yspeed === -y) return;
-    this.xspeed = x;
-    this.yspeed = y;  
+    this.tail = []; 
   }
 
   eat(pos) {
-    let d = dist(this.x, this.y, pos.x, pos.y);
+    var d = dist(this.x, this.y, pos.x, pos.y);
+
     if (d < 1) {
       this.total++;
       return true;
-    }
-    return false;
+    } else {
+      return false;
+    }    
   }
 
+  direction(x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+  
+  gameOver() {
+    noLoop();
+    fill('black');
+    textFont('Franklin Gothic', 90);
+    text("Game Over", 35, 250);
+    textFont('Franklin Gothic', 25);
+    text("(Press the space key to begin)", 90, 290);
+  }  
+
   death() {
-    if (this.x < 0 || this.x >= width || this.y < 0 || this.y >= height) {
-      this.gameOver();
-      return;
-    }
-    for (let i = 0; i < this.tail.length; i++) {
-      let pos = this.tail[i];
-      let d = dist(this.x, this.y, pos.x, pos.y);
+    for (var i = 0; i < this.tail.length; i++) {
+      var pos = this.tail[i];
+      var d = dist(this.x, this.y, pos.x, pos.y);
+
       if (d < 1) {
         this.gameOver();
       }
     }
   }
 
-  gameOver() {
-    gameIsPlaying = false; // Updates our custom state tracking variable
-    noLoop();
-    fill(255, 0, 0);
-    textSize(32);
-    textAlign(CENTER, CENTER);
-    text("GAME OVER", width / 2, height / 2 - 20);
-    textSize(16);
-    fill(0);
-    text("Press Spacebar or Tap Screen to Restart", width / 2, height / 2 + 20);
-  }
-
   update() {
-    if (this.total === this.tail.length) {
-      for (let i = 0; i < this.tail.length - 1; i++) {
-        this.tail[i] = this.tail[i + 1];
-      }
+    for (var i = 0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i + 1];
     }
-    this.tail[this.total - 1] = createVector(this.x, this.y);
 
-    this.x += this.xspeed * scl;
-    this.y += this.yspeed * scl;
+    if (this.total >= 1) {
+      this.tail[this.total - 1] = createVector(this.x, this.y);
+    }
+
+    this.x = this.x + this.xspeed * scl;
+    this.y = this.y + this.yspeed * scl;
+
+    this.x = constrain(this.x, 0, width - scl);
+    this.y = constrain(this.y, 0, height - scl);
   }
 
   show() {
-    fill(46, 204, 113); 
-    noStroke();
-    for (let i = 0; i < this.tail.length; i++) {
+    fill('orange');
+
+    for (var i = 0; i < this.tail.length; i++) {
       rect(this.tail[i].x, this.tail[i].y, scl, scl);
     }
-    fill(39, 174, 96); 
+
+    rect(this.x, this.y, scl, scl);
+  }
+}
+
+class Food {
+  constructor() {
+    this.pickLocation();
+  }
+
+  pickLocation() {
+    var cols = floor(width / scl);
+    var rows = floor(height / scl);
+
+    this.x = floor(random(cols)) * scl;
+    this.y = floor(random(rows)) * scl;
+  }
+
+  show() {
+    fill('gray');
     rect(this.x, this.y, scl, scl);
   }
 }
